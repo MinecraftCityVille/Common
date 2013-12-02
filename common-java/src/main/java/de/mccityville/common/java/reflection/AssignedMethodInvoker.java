ba4d06605f912151c9658eb38037e3c69059effb
maxikg <maxmax58@googlemail.com>
2013-12-02 03:32:13 +0100

    Equals überschrieben

--- common-java/src/main/java/de/mccityville/common/java/reflection/AssignedMethodInvoker.java
+++ common-java/src/main/java/de/mccityville/common/java/reflection/AssignedMethodInvoker.java
@@ -3,6 +3,7 @@ package de.mccityville.common.java.reflection;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
+import com.google.common.base.Objects;
 import com.google.common.base.Preconditions;
 
 public class AssignedMethodInvoker {
@@ -22,5 +23,21 @@ public class AssignedMethodInvoker {
 			method.setAccessible(true);
 		
 		return method.invoke(target, args);
+	}
+	
+	@Override
+	public boolean equals(Object other) {
+		if (!(other instanceof AssignedMethodInvoker))
+			return false;
+		
+		AssignedMethodInvoker invoker = (AssignedMethodInvoker) other;
+		
+		if (!Objects.equal(target, invoker.target))
+			return false;
+		
+		if (!Objects.equal(method, invoker.method))
+			return false;
+		
+		return true;
 	}
 }
