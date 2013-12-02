457f5d2c90bc1597dc3ab39ab9331576013d9ea8
maxikg <maxmax58@googlemail.com>
2013-12-02 03:13:48 +0100

    ImmutableUtils für Google Guava hinzugefügt

---
+++ common-java/src/main/java/de/mccityville/common/java/collections/ImmutableUtils.java
@@ -0,0 +1,50 @@
+package de.mccityville.common.java.collections;
+
+import java.util.Collection;
+import java.util.Map;
+
+import com.google.common.collect.ImmutableBiMap;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+
+public final class ImmutableUtils {
+	
+	public static <T> ImmutableList<T> immutableList(Collection<T> collection) {
+		ImmutableList.Builder<T> builder = ImmutableList.builder();
+		
+		if (collection.size() > 0)
+			builder.addAll(collection);
+		
+		return builder.build();
+	}
+	
+	public static <T> ImmutableSet<T> immutableSet(Collection<T> collection) {
+		ImmutableSet.Builder<T> builder = ImmutableSet.builder();
+		
+		if (collection.size() > 0)
+			builder.addAll(collection);
+		
+		return builder.build();
+	}
+	
+	public static <K, V> ImmutableMap<K, V> immutableMap(Map<K, V> map) {
+		ImmutableMap.Builder<K, V> builder = ImmutableMap.builder();
+		
+		if (map.size() > 0)
+			builder.putAll(map);
+		
+		return builder.build();
+	}
+	
+	public static <K, V> ImmutableBiMap<K, V> immutableBiMap(Map<K, V> map) {
+		ImmutableBiMap.Builder<K, V> builder = ImmutableBiMap.builder();
+		
+		if (map.size() > 0)
+			builder.putAll(map);
+		
+		return builder.build();
+	}
+	
+	private ImmutableUtils() { }
+}
